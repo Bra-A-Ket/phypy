f3b85fba6aa592520b645e5796264777fb494b2e
Bra-A-Ket <80975351+Bra-A-Ket@users.noreply.github.com>
2022-09-26 08:56:52 +0200

    remove pycache

--- calculations.py
+++ calculations.py
@@ -1,7 +1,7 @@
 from imports import *
 
 def main():
-    names = ["phi_1", "phi_2", "phi_3", "phi_3"]
+    names = ["phi_1", "phi_2", "phi_3", "phi_3", "phi_3", "phi_4", "phi_4", "phi_4"]
     fields = [RealScalarField4D(name=name) for name in names]
     WickContraction(fields=fields)
 if __name__ == "__main__":
